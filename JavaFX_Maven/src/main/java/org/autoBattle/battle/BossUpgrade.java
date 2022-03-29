8636b10ac39e6af3755f541c05d4ca3bc9d878f0
Daniel Smidstrup <71447909+Da9el00@users.noreply.github.com>
2022-03-29 17:24:42 +0200

    Add files via upload

---
+++ JavaFX_Maven/src/main/java/org/autoBattle/battle/BossUpgrade.java
@@ -0,0 +1,20 @@
+package org.autoBattle.battle;
+
+public class BossUpgrade {
+
+    private boolean everyTurnTrigger;
+
+    public BossUpgrade(boolean everyTurnTrigger) {
+        this.everyTurnTrigger = everyTurnTrigger;
+    }
+
+    public void triggerEveryTurn(){
+    }
+
+    public void triggerWhenPicked(){
+    }
+
+    public boolean isEveryTurnTrigger() {
+        return everyTurnTrigger;
+    }
+}
---
+++ JavaFX_Maven/src/main/java/org/autoBattle/fighters/Fighter.java
@@ -0,0 +1,86 @@
+package org.autoBattle.fighters;
+
+import javafx.scene.control.ProgressBar;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.util.Random;
+
+public abstract class Fighter {
+     private final Rectangle rectangle;
+     private int maxHealth;
+     private Damage damage;
+     private int currentHealth;
+     private ProgressBar healthBar;
+     private final Text healthText;
+     protected Random random = new Random();
+
+    public Fighter(Rectangle rectangle, int maxHealth, int damage) {
+        this.rectangle = rectangle;
+        this.maxHealth = maxHealth;
+        this.damage = new Damage(damage);
+        healthBar = new ProgressBar();
+        currentHealth = this.maxHealth;
+        healthText = new Text();
+    }
+
+    public Text getHealthText() {
+        return healthText;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void resetHealth(){
+        currentHealth = maxHealth;
+        updateHealth();
+    }
+
+    public int getDamage() {
+        return damage.getDamage();
+    }
+
+    public Damage getDamageObject(){
+        return damage;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    protected void lowerCurrentHealth(int damage){
+        currentHealth -= damage;
+    }
+
+    public void changeCurrentHealth (int healAmount){
+        currentHealth += healAmount;
+    }
+
+    public ProgressBar getHealthBar() {
+        return healthBar;
+    }
+
+    public abstract int takeDamage(int damage, Damage damageObject);
+
+    public abstract void removeSquareEnemy();
+
+    public abstract void updateHealth();
+
+    public boolean isDead(){
+        return currentHealth <= 0;
+    }
+
+    public void upgradeMaxHealth(int upgradeAmount){
+        maxHealth += upgradeAmount;
+    }
+
+    public void upgradeDamage(int upgradeAmount){
+        damage.increaseDamage(upgradeAmount);
+    }
+
+}
